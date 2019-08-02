f839c1a2f844c6daf25d32f5a85ba2b2b16e5d5d
Danilo Rocha <danilontfs@gmail.com>
2019-08-01 22:47:55 -0300

    Configurando banco de dados.

---
+++ app/Cliente.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    protected $table = 'cliente';
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+      'nome','profissao'
+    ];
+}
---
+++ database/migrations/2019_08_02_010454_create_cliente_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClienteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->string('nome',100);
+            $table->string('profissao',250)->nullable(true);
+
+            //CONFIGURAÇÕES DO BANCO
+            $table->engine    = 'InnoDB';
+            $table->charset   = 'utf8';
+            $table->collation = 'utf8_general_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::dropIfExists('cliente');
+    }
+}
